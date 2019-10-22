f7a85f77f3d9f41c170a110ac1c661350d8f6e4c
dumazert <rebeccadumazert@hotmail.fr>
2019-10-22 11:05:03 +0200

    first commit

---
+++ scripts/userQueries.js
@@ -0,0 +1,6 @@
+// i ask name/second name/age at user and i display responses in console
+const firstName = prompt("quel est votre nom ?")
+const secondName = prompt("quel est votre prénom?")
+const age = prompt("quel est votre âge?")
+
+console.log("Bonjour je m'appelle " + firstName +" "+secondName+" "+"j'ai"+" "+age+" "+"ans")
